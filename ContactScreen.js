c5a4e9cf0a3cce124e7248ffd2dae50d2f17f6d0
Tardieh <tardiehbismark@hotmail.com>
2021-04-13 02:16:06 -0400

    Display Contact list

---
+++ ContactScreen.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {View, FlatList, Text} from "react-native"
+import Contact from './components/Contact'
+
+export default function ContactScreen() {
+    const contacts = [
+        {name: 'James Luke', number: '903-922-1029'},
+        {name: 'Fufu Soup', number: '320-782-9129'},
+        {name: 'Rice Stew', number: '910-612-9132'},
+        {name: 'Jazz Puke', number: '832-102-4072'},
+        {name: 'Debs Olina', number: '240-021-8910'},
+        {name: 'Meeky Pangbe', number: '723-093-9833'}
+    ]
+
+
+    return (
+        <View>
+            <FlatList 
+                data={contacts}
+                renderItem={({ item }) => {
+                    return <Contact name={item.name} phone={item.number} />
+                }}
+                keyExtractor={(item)=>item.number} 
+            />
+        </View>
+    )
+}
